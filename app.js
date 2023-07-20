269e22ee4e950ff768aa557d38761d66c37efc9f
daniyel avnilov <danavn34@gmail.com>
2023-07-20 15:15:59 -0700

    completed terminology word list

---
+++ app.js
@@ -0,0 +1,4 @@
+// aviation word list: https://www.linkedin.com/pulse/100-most-common-aviation-english-words-used-pilots-petter-zugaib/
+
+const aviation = ["jumpseat","groundcontrol","altitude","waypoint","visibility","velocity","vector","unicom","turbulence","transponder","tower","thrust","takeoff","runway","range","radar","navigation","missedapproach","maneuver","jetway","groundspeed","glide","fuel","flaps","elevation","descent","crosswind","cockpit","birdstrike","beacon","autopilot","airspeed","accelerate"]
+
