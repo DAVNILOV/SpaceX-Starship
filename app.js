178c4173864c452c3b5c4e127417b1ca6a704411
daniyel avnilov <danavn34@gmail.com>
2023-07-21 16:38:00 -0700

    project completed

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // aviation word list: https://www.linkedin.com/pulse/100-most-common-aviation-english-words-used-pilots-petter-zugaib/
 
 const wordList = {
-    aviation: ["jumpseat","groundcontrol","altitude","waypoint","visibility","velocity","vector","unicom","turbulence","transponder","tower","thrust","takeoff","runway","range","radar","navigation","missedapproach","maneuver","jetway","groundspeed","glide","fuel","flaps","elevation","descent","crosswind","cockpit","birdstrike","beacon","autopilot","airspeed","accelerate"]
+    aviation: ["jumpseat","altitude","waypoint","visibility","velocity","vector","unicom","turbulence","transponder","tower","thrust","takeoff","runway","range","radar","navigation","maneuver","jetway","groundspeed","glide","fuel","flaps","elevation","descent","crosswind","cockpit","birdstrike","beacon","autopilot","airspeed","accelerate"]
 }
 
+let count = 1
+let numLetters = 0
+let guessedLetters = []
+let gameover = false
 
 function getWord (words) {
     console.log(words)
@@ -11,27 +15,66 @@ function getWord (words) {
     const word = words[wordIndex];
     return word
 }
-const word = getWord(wordList.aviation);
+let word = getWord(wordList.aviation);
+console.log(word)
+let guess = "";
 
-
+// the empty dashes
+for (let i = 0; i < word.length; i ++) {
+    document.getElementById("word").innerHTML += "-"
+    guess += "-"
+}
 
 
 function setLetter(letter) {
+    if (!guessedLetters.includes(letter) && !gameover) {
+
+    
     //check if letter is in word, 
     console.log(letter)
+    guessedLetters.push(letter)
     let tableClass = ' '
     if (word.includes(letter.toLowerCase())){
-         tableClass = "correct-letter" 
+         tableClass = "correct-letter"
+          let index = word.indexOf(letter.toLowerCase())
+          console.log(index)
+          while (index != -1) {
+            guess = guess.substring(0, index) + letter.toLowerCase() + guess.substring(index + 1)
+            console.log(guess)
+            document.getElementById("word").innerHTML = guess
+            word = word.substring(0, index) + "-" + word.
+            substring(index + 1)
+            console.log(word)
+            numLetters ++
+            index = word.indexOf(letter.toLowerCase())
+            console.log(index)
+          }
+
+          if (numLetters == word.length) {
+            alert("You guessed the word correctly")
+            gameover = true;
+          }
     } else {
          tableClass = "incorrect-letter"
+         if (count <= 6) {
+            let starship = document.getElementById("starship")
+            let img = document.createElement("img")
+            img.src="STARSHIP/PART" + count + ".png"
+            count ++;
+           starship.appendChild(img)
+           if (count == 7) {
+            alert("Game Over")
+            gameover = true;
+           }
+         } 
     } 
     const tableRow = createTablerow(tableClass, letter)
     document.getElementById("used-letters").appendChild(tableRow)
-
+    }
 }
 
     function createTablerow (tableClass, letter) {
-        const rowEl = document.createElement("tr")
+        const rowEl = document.getElementById(tableClass)
         const dataTableEl = document.createElement("td")
         dataTableEl.innerText = letter
         dataTableEl.className = tableClass
@@ -39,12 +82,43 @@ function setLetter(letter) {
         return rowEl
     } 
 
-    createTablerow('test', 'V')
-    //set a variable for the class based on correct or not.
-    //create a table row and TD(table data)
-    //add a row to the table
-    // if correct add class correct and guess anther letter, and if incorrect red
-       // display (correct guess or incorrect guess)
-//     document.getElementById('name').innerHTML = document.getElementById('name').innerHTML + letter;
+function reset () {
+ count = 1
+ numLetters = 0
+ guessedLetters = []
+ gameover = false
 
 
+word = getWord(wordList.aviation);
+console.log(word)
+guess = "";
+document.getElementById("word").innerHTML = ""
+// the empty dashes
+for (let i = 0; i < word.length; i ++) {
+    document.getElementById("word").innerHTML += "-"
+    guess += "-"
+}
+
+let rowEl = document.getElementById("correct-letter")
+while (rowEl.firstChild) {
+    rowEl.removeChild(rowEl.firstChild)
+}
+let dataTableEl = document.createElement("th")
+dataTableEl.innerText = "ORBIT CORRECT LETTERS"
+dataTableEl.className = "correct-letters"
+rowEl.appendChild(dataTableEl)
+
+rowEl = document.getElementById("incorrect-letter")
+while (rowEl.firstChild) {
+    rowEl.removeChild(rowEl.firstChild)
+}
+dataTableEl = document.createElement("th")
+dataTableEl.innerText = "ORBIT INCORRECT LETTERS"
+dataTableEl.className = "incorrect-letters"
+rowEl.appendChild(dataTableEl)
+
+rowEl = document.getElementById("starship")
+while (rowEl.firstChild) {
+    rowEl.removeChild(rowEl.firstChild)
+}
+}
